aa63a233659389f205cbfe817f8d70d00e0242ec
Mostafa Mansour <mostafa@outlook.kr>
2019-09-05 14:10:28 +0400

    added appflow sdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PreviewAnyFile } from '@ionic-native/preview-any-file/ngx';
+import { IOSFilePicker } from '@ionic-native/file-picker/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,8 +21,13 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    PreviewAnyFile,
+    FilePath,
+    IOSFilePicker,
+    FileChooser
+    ,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { PreviewAnyFile } from '@ionic-native/preview-any-file/ngx';
+import { IOSFilePicker } from '@ionic-native/file-picker/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+import { FileChooser } from '@ionic-native/file-chooser/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  uri: any = "https://file-examples.com/wp-content/uploads/2017/10/file-example_PDF_1MB.pdf";
+  constructor(
+    private filePicker: IOSFilePicker,
+    private previewAnyFile: PreviewAnyFile,
+    private filePath: FilePath,
+    private fileChooser: FileChooser
+  ) { }
 
-  constructor() {}
+  onClick() {
+    this.previewAnyFile.preview(this.uri).then(uri => {
+      console.log('preview succ', uri)
+
+    })
+      .catch(err => console.log('preview Error', err));;
+  }
+
+  select() {
+    // this.filePicker.pickFile()
+    //   .then(uri => {
+    //     console.log('uri', uri)
+    //     //this.absLink(uri)
+    //     this.uri = uri
+    //   })
+    //   .catch(err => console.log('Error', err));
+
+
+    this.fileChooser.open()
+      .then(uri => {
+        console.log(uri)
+        //this.uri = uri
+        this.filePath.resolveNativePath(uri)
+          .then(filePath => { console.log(filePath); this.uri = filePath })
+          .catch(err => console.log(err))
+
+      })
+      .catch(e => console.log(e));
+
+  }
 
 }
